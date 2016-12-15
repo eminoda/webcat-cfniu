3e78b6a84ab0e448e29ece9b90f09f74bc79228f
sxh@9niu.com <245978782@qq.com>
2016-12-15 17:32:13 +0800

    + wx build

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-var gulp = require('gulp'),
+var gulp = require('gulp-help')(require('gulp')),
   rename = require('gulp-rename'),
   clean = require('gulp-clean'),
   sequence = require('gulp-sequence'),
@@ -7,26 +7,98 @@ var gulp = require('gulp'),
   sass = require('gulp-sass'),
   proxy = require('http-proxy-middleware'),
   bs = require('browser-sync').create(),
-  package = require('./package.json');
+  argv = require('minimist')(process.argv.slice(2)),
+  notify = require("gulp-notify"),
+  path = require('path'),
+  fs = require('fs'),
+  colors = require('colors'),
+  program = require('commander'),
+  Promise = require('bluebird');
+package = require('./package.json');
 
 // 构建项目
-gulp.task('build', sequence('clean', ['build-extname-wxml', 'build-extname-wxss', 'build-js', 'build-json'],'watch'));
+gulp.task('build', '构建项目>>> gulp build', sequence('clean', ['build-extname-wxml', 'build-extname-wxss', 'build-js', 'build-json'], 'watch'));
 
 //监听项目
-gulp.task('watch', function() {
+gulp.task('watch', '项目监听>>> gulp watch', function() {
   gulp.watch(['./src/**/**/*.html', './src/**/*.html', './src/*.html'], ['build-extname-wxml']);
   gulp.watch(['./src/**/**/*.scss', './src/**/*.scss', './src/*.scss'], ['build-extname-wxss']);
   gulp.watch(['./src/**/**/*.js', './src/**/*.js', './src/*.js'], ['build-js']);
   gulp.watch(['./src/**/**/*.json', './src/**/*.json', './src/*.json'], ['build-json']);
 });
+
 // 清理项目
-gulp.task('clean', function() {
-  return gulp.src(['pages', './*.log', './app.wxss', './app.json', './app.js', './app.wxml'], { read: false })
+gulp.task('clean', '清理项目>>> gulp clean', function() {
+  return gulp.src(['pages', './*.log', './app.wxss', './app.json', './app.js', './app.wxml'])
     .pipe(clean());
 });
 
+gulp.task('add', '添加文件>>> gulp add --dir=./src/pages/test --name=test', function() {
+  var dir = argv.dir;
+  var name = argv.name;
+  if (!dir || !name) {
+    printError('参数错误，例如：gulp add --dir=./src/pages/test --name=test');
+  } else {
+    createDir(dir).then(function() {
+      var filesFun = [];
+      console.log('待添加文件==>>'.green + colors.green(package.gulpConfig.wxExt));
+      for (var ext of package.gulpConfig.wxExt) {
+        filesFun.push(createFile(dir, '/' + name + ext));
+      }
+      Promise.all(filesFun).then(function() {
+        return true;
+      })
+    }, function(err) {
+      printError(err);
+    })
+  }
+});
+
+function createDir(dir) {
+  try {
+    return new Promise(function(resolve, reject) {
+      if (!fs.existsSync(dir)) {
+        console.log('开始创建路径==>' + dir);
+        var pathtmp;
+        dir.split('/').forEach(function(dirname) {
+          if (pathtmp) {
+            pathtmp = path.join(pathtmp, dirname);
+          } else {
+            pathtmp = dirname;
+          }
+          if (!fs.existsSync(pathtmp)) {
+            console.log('开始创建路径==>' + pathtmp);
+            if (!fs.mkdirSync(pathtmp)) {
+              return false;
+            }
+          }
+        });
+        resolve();
+      }else{
+        reject(dir+'目录已存在');
+      }
+    })
+  } catch (e) {
+    printError(e);
+    resolve();
+  }
+}
+
+function createFile(path, name) {
+  try {
+    console.log('开始创建==>'.green + path.green + name.green);
+    return fs.writeFileSync(path + name);
+  } catch (e) {
+    printError(e);
+  }
+}
+
+function printError(err) {
+  console.error(colors.red(err));
+  return false;
+}
 // 本地开发服务模拟
-gulp.task('server', ['build'], function() {
+gulp.task('server', '启动服务>>>todo', ['build'], function() {
   // 代理配置2
   var context = ['**/*.api*', '/common/**'],
     options = {
